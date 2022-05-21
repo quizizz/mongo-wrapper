b719b542a2e07ee823c1062724c32fa81b4e6588
Praveen Kumar <praveen@quizizz.com>
2022-05-21 16:02:51 +0530

    fix(types):  added HydratedDocument type for new project template model types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,7 @@
 
 export * as Mongoose from 'mongoose';
 export * as Mongo from 'mongodb';
-
+export { HydratedDocument } from 'mongoose/types/index'
 export * from './mongo';
 export *  from './mongoose';
 export * from './utils';
